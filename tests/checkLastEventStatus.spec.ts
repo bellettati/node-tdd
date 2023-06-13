8dc780ebfe50905b85cb7294e74a1e1ef468710f
john.bellettati <john.bellettati@simchef.net>
2023-06-13 15:11:48 -0300

    ensuring that CheckLastEventStatus calls LastLoadEventRepo only once

--- tests/checkLastEventStatus.spec.ts
+++ tests/checkLastEventStatus.spec.ts
@@ -16,9 +16,11 @@ interface LoadLastEventRepository {
 
 class LoadLastEventRepositoryMock implements LoadLastEventRepository {
     groupId: string
+    callsCount = 0
 
     async loadLastEvent(groupId: string): Promise<void> {
         this.groupId = groupId
+        this.callsCount++
     }
 }
 
@@ -30,5 +32,6 @@ describe('CheckLastEventStatus', () => {
         await checkLastEventStatus.execute('any_group_id')
 
         expect(loadLastEventRepository.groupId).toBe('any_group_id')
+        expect(loadLastEventRepository.callsCount).toBe(1)
     })
 })
